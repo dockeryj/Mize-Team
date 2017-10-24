babaeb730f3dc8f20735eb7b67976a93030a2523
dockeryj <dockeryj19@mail.wlu.edu>
2017-10-24 19:35:01 -0400

    !

--- project5/src/MainView.java
+++ project5/src/MainView.java
@@ -70,7 +70,7 @@ public class MainView extends JFrame{
     private class MoveListener implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
 			model.step();
-			statsField.setText(model.toString());
+			System.out.println(model.toString());
 			//p1Card.setCard();
 			//p2Card.setCard();
 			//model.step needs to return in some way a card image to be displayed. Also,
