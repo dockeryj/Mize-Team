2e0ba04bd060778fe7f7376dd04c5213963d0c5e
surranc20@mail.wlu.edu <chris@chriss-mbp.ad.wlu.edu>
2017-11-04 14:00:13 -0400

    s

---
+++ project5/src/AbstractPIle.java
@@ -0,0 +1,14 @@
+/**
+ * Forms the basis of all piles in the freecell game
+ * @author Chris Surran
+ *
+ */
+
+public abstract class AbstractPile implements Pile, Iterable<Card>{
+	
+	
+	
+	
+	
+	
+}
