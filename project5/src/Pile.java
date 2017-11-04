cecddf6eda9046a6955f364d44bac11968d4341c
surranc20@mail.wlu.edu <chris@chriss-mbp.ad.wlu.edu>
2017-11-04 15:22:53 -0400

    More work on abstract pile

--- project5/src/Pile.java
+++ project5/src/Pile.java
@@ -2,11 +2,12 @@ import java.util.Iterator;
 public interface Pile {
 	public abstract String toString();
 	public abstract int size(); //
+	public abstract int maxSize();
 	public abstract void clear(); //
 	public abstract void add(Card cardToBeAdded); //
 	public abstract Card remove(); //
-	public abstract boolean transfer(Pile targetPile);
-	public abstract boolean canTransfer(Pile targetPile);
+	public abstract boolean transfer(Pile targetPile); //
+	public abstract boolean canTransfer(Pile targetPile); //
 	public abstract boolean isEmpty();
 	public abstract Iterator<Card> iterator(); // This is because we will need to iterate through pile to display gui
 }
