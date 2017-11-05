0922831582f87fbdc7e460fe990b8f2046bc0262
surranc20@mail.wlu.edu <chris@chriss-mbp.ad.wlu.edu>
2017-11-05 17:58:51 -0500

    a

--- project5/src/Tableau.java
+++ project5/src/Tableau.java
@@ -12,39 +12,6 @@ public class Tableau extends AbstractPile{
 		super(52); // 52 because we do not care if to many cards end up in one pile after start
 	}
 	
-	public boolean inOrder(int low, int high) { // remember that size -1 is the top.
-		int nextRank = -1;
-		if (low < 0 || high > size() - 1) throw new IllegalStateException("Illegalish index");
-		ArrayList<Suit> possibleSuits = new ArrayList<>();
-		for(int i = low; i <= high; i++) {
-			Card c = get(i);
-			if (nextRank == -1) { // this will only trigger for first card
-				possibleSuits = getNextSuit(c);
-				nextRank = c.getRank() - 1;}
-			else { 
-				if (!(c.getRank() == nextRank) || (!possibleSuits.contains(c.getSuit()))) {
-				System.out.println("Card: " + c +"; PossibleSuits: " + possibleSuits + "; Next Rank: " + nextRank);
-				return false;
-				}
-				possibleSuits = getNextSuit(c);
-				nextRank = c.getRank() - 1;
-			
-			}
-		}
-		return true; // return true if you make it all the way through the pile
-	}
-
-	public ArrayList<Suit> getNextSuit(Card c){
-		ArrayList<Suit> possibleSuits = new ArrayList<>();
-		if ((c.getSuit().equals(Suit.club)) || (c.getSuit().equals(Suit.spade))){ // next card must be red
-			possibleSuits.add(Suit.diamond);
-			possibleSuits.add(Suit.heart);}
-		else {
-			possibleSuits.add(Suit.club);
-			possibleSuits.add(Suit.spade);}
-		return possibleSuits;
-	}
-	
 	
 	public boolean canTransfer(Pile sourcePile, int low, int high) {
 		if (! super.canTransfer(sourcePile)) return false;
