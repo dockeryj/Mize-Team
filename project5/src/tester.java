aacc297318d4a7e5b6c7294c04dc77a74417fa20
robymize <mizer20@mail.wlu.edu>
2017-11-05 18:32:46 -0500

    tester

--- project5/src/tester.java
+++ project5/src/tester.java
@@ -75,7 +75,8 @@ public class tester {
 		tableau5.add(g);
 		tableau4.transfer(tableau5, 0, 0);
 		System.out.println("Tableau 4 after transfer: \n" + tableau4);
-		System.out.println("Tableau 5 after transfer: \n" + tableau5);
+		System.out.println("Tableau 5 after transfer (Expect 2 of clubs): \n" + tableau5);
+		
 		
 		
 		
