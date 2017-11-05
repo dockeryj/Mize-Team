7c7ffdad7c46912ca202bb39e46b67f19bf683b0
surranc20@mail.wlu.edu <chris@chriss-mbp.ad.wlu.edu>
2017-11-05 17:49:00 -0500

    k

--- project5/src/Tableau.java
+++ project5/src/Tableau.java
@@ -47,13 +47,13 @@ public class Tableau extends AbstractPile{
 	
 	
 	public boolean canTransfer(Pile sourcePile, int low, int high) {
-		if (low < 0 || high > sourcePile.size() - 1) throw new IllegalStateException("Illegal index");
 		if (! super.canTransfer(sourcePile)) return false;
 		if (! inOrder(low, high)) return false;
 		return true;
 	}
 	
 	public boolean transfer(Pile sourcePile, int low, int high) {
+		if (low < 0 || high > sourcePile.size() - 1) throw new IllegalStateException("Illegal!!!! index");
 		if(! canTransfer(sourcePile, low, high)) return false;
 		//ArrayList<Card> cardsToAdd = new ArrayList<>();
 		for (int i = low; i <= high; i++) this.add(sourcePile.remove(i));
