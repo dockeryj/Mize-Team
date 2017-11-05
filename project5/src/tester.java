a61cbaf8319132ade5a47ad82424d91afe86d4e7
surranc20@mail.wlu.edu <chris@chriss-mbp.ad.wlu.edu>
2017-11-05 16:31:52 -0500

    uploaded tester

--- project5/src/tester.java
+++ project5/src/tester.java
@@ -1,21 +1,21 @@
+import java.util.ArrayList;
 public class tester {
 
 	public static void main(String[] args) {
 		FreeCellPile cell = new FreeCellPile();
 		FreeCellPile cell3 = new FreeCellPile();
 		HomeCellPile cell2 = new HomeCellPile();
+		Tableau t1 = new Tableau();
 		for (int i = 1; i < 14; i++) 
 			{
 			Card c = new Card(Suit.club, i);
-			cell.add(c);
+			t1.add(c);
 			System.out.println(cell2.transfer(cell));
 			}
-//		System.out.println(cell2);
-//		HomeCellPile cell3 = new HomeCellPile();
-//		Card c = new Card(Suit.club, 2);
-//		cell.add(c);
-//		System.out.println(cell);
-		System.out.println(cell.canTransfer(cell2));
+		ArrayList<Card> cardsToCheck = new ArrayList<>();
+		for (Card c: t1) cardsToCheck.add(c);
+		t1.
+		
 		
 	}
 
