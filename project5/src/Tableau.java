203e4f0d0334ed1761adfe9c243ac5631e6c3c53
surranc20@mail.wlu.edu <chris@chriss-mbp.ad.wlu.edu>
2017-11-05 16:11:13 -0500

    a

--- project5/src/Tableau.java
+++ project5/src/Tableau.java
@@ -14,22 +14,26 @@ public class Tableau extends AbstractPile{
 	
 	public boolean inOrder(ArrayList<Card> cards) { // remember that size -1 is the top.
 		int nextRank = -1;
-		ArrayList<Suit> possibleSuits = new ArrayList();
+		ArrayList<Suit> possibleSuits = new ArrayList<>();
 		for(Card c: cards) {
 			if (nextRank == -1) { // this will only trigger for first card
-				nextRank = c.getRank() - 1;
-				if ((c.getSuit().equals(Suit.club)) || (c.getSuit().equals(Suit.spade))){ // next card must be red
-					possibleSuits.clear();
-					possibleSuits.add(Suit.diamond);
-					possibleSuits.add(Suit.heart);
-				}
-				
-				
+				possibleSuits = getNextSuit(c);
+				nextRank = c.getRank() - 1;}
+			
+			else if (!(c.getRank() == nextRank) || !(possibleSuits.contains(c.getSuit()))) return false;
 			}
-			
-			
-		}
-		return true; // place holder
+		return true; // return true if you make it all the way through the pile
+	}
+
+	public ArrayList<Suit> getNextSuit(Card c){
+		ArrayList<Suit> possibleSuits = new ArrayList<>();
+		if ((c.getSuit().equals(Suit.club)) || (c.getSuit().equals(Suit.spade))){ // next card must be red
+			possibleSuits.add(Suit.diamond);
+			possibleSuits.add(Suit.heart);}
+		else {
+			possibleSuits.add(Suit.club);
+			possibleSuits.add(Suit.spade);}
+		return possibleSuits;
 	}
 	
 	
