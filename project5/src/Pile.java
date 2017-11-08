fb9c687551d7b5f29d16f7f3950634e3e0a93ae5
dockeryj <dockeryj19@mail.wlu.edu>
2017-11-08 13:47:11 -0500

    fixed abstract thing

--- project5/src/Pile.java
+++ project5/src/Pile.java
@@ -12,44 +12,44 @@ public interface Pile {
      * Returns a string representation of the pile.
      * @return String representation. 
      */
-	public abstract String toString();
+	public String toString();
 	
 	/**
      * Returns the size of the pile.
      * @return The size of the pile.
      */
-	public abstract int size(); 
+	public int size(); 
 	
 	/**
      * Returns the max size of the pile.
      * @return The max size of the pile.
      */
-	public abstract int maxSize(); 
+	public int maxSize(); 
 	
 	/**
      * Returns the card at the top of the pile.
      * @throws IllegalStateException if cell is empty.
      * @return Top card.
      */
-	public abstract Card getTop(); 
+	public Card getTop(); 
 	
 	/**
      * Clears the pile.
      */
-	public abstract void clear(); 
+	public void clear(); 
 	
 	/**
      * Adds a card to the pile.
      * @param cardToBeAdded The card to be added.
      */
-	public abstract void add(Card cardToBeAdded); 
+	public void add(Card cardToBeAdded); 
 	
 	/**
      * Removes the card at the top of the pile.
      * @throws IllegalStateException if cell is empty.
      * @return Removed card.
      */
-	public abstract Card remove(); 
+	public Card remove(); 
 	
 	/**
      * Removes the card at a given index.
@@ -57,33 +57,33 @@ public interface Pile {
      * @throws IllegalStateException if index is out of bounds.
      * @return Removed card.
      */
-	public abstract Card remove(int i);
+	public Card remove(int i);
 	
 	/**
      * Transfers a card to a source pile
      * @param sourcePile the source pile. 
      * @return True if card is transferred.
      */
-	public abstract boolean transfer(Pile sourcePile); 
+	public boolean transfer(Pile sourcePile); 
 	
 	/**
      * Determines if a card can be transferred to a source pile
      * @param sourcePile the source pile. 
      * @return True if card can be transferred.
      */
-	public abstract boolean canTransfer(Pile sourcePile); 
+	public boolean canTransfer(Pile sourcePile); 
 	
 	/**
      * Determines if a pile is empty.
      * @return True if pile is empty.
      */
-	public abstract boolean isEmpty(); 
+	public boolean isEmpty(); 
 	
 	/**
      * Creates a card type iterator for the pile.
      * @return Iterator for the pile. 
      */
-	public abstract Iterator<Card> iterator(); 
+	public Iterator<Card> iterator(); 
 	
 	/**
      * Checks to see if the cards in a subsection of a pile are in order
@@ -92,7 +92,7 @@ public interface Pile {
      * @throws IllegalStateException if index is out of bounds. 
      * @return true if they are in order
      */
-	public abstract boolean inOrder(int low, int high);
+	public boolean inOrder(int low, int high);
 	
 	/**
      * Retrieves a card at a given index. 
@@ -100,12 +100,12 @@ public interface Pile {
      * @throws IllegalStateException if index is out of bounds. 
      * @return Card at given index. 
      */
-	public abstract Card get(int i);
+	public Card get(int i);
 	
 	/**
      * Returns an array list of acceptable suits for the next card
      * @param c Card
      * @return ArrayList<Suit> containing next acceptable suits
      */
-	public abstract ArrayList<Suit> getNextSuit(Card c);
+	public ArrayList<Suit> getNextSuit(Card c);
 }
